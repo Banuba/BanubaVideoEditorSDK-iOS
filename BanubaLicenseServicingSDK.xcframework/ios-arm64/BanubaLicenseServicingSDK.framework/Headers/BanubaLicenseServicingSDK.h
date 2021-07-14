e026aa1c23ab8ed1f8d6bb63e1acd0f63ae546d6
Gleb Markin <gleb.markin@banuba.com>
2021-07-14 12:46:57 +0300

    0.7.beta

--- BanubaLicenseServicingSDK.xcframework/ios-arm64/BanubaLicenseServicingSDK.framework/Headers/BanubaLicenseServicingSDK.h
+++ BanubaLicenseServicingSDK.xcframework/ios-arm64/BanubaLicenseServicingSDK.framework/Headers/BanubaLicenseServicingSDK.h
@@ -15,5 +15,4 @@ FOUNDATION_EXPORT const unsigned char BanubaLicenseServicingSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BanubaLicenseServicingSDK/PublicHeader.h>
 
-#import <BanubaLicenseServicingSDK/BanubaEffectPlayerInteractor.h>
 
