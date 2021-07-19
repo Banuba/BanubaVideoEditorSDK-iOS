97809a233da26403eecea0b72ee1434917e7a180
Gleb Markin <gleb.markin@banuba.com>
2021-07-19 15:43:52 +0300

    1.0.17.native-script

---
+++ BanubaVideoEditorSDK.xcframework/ios-arm64_x86_64-simulator/BanubaVideoEditorSDK.framework/Headers/BanubaSDKFrameworkInteractor.h
@@ -0,0 +1,29 @@
+//
+//  BanubaSDKFrameworkInteractor.h
+//  BanubaVideoEditorSDK
+//
+//  Created by Gleb Markin on 2/24/21.
+//  Copyright © 2021 Banuba. All rights reserved.
+//
+
+@import AVKit;
+
+NS_ASSUME_NONNULL_BEGIN
+NS_SWIFT_NAME(BanubaSDKFrameworkInteractor)
+
+// MARK: - Banuba Sdk framework interactor
+__attribute__((weak_import)) @interface BanubaSDKFrameworkInteractor : NSObject
+
++ (BOOL)isFrameworkAvailable;
+
++ (void) initializeCameraModule: (NSString * _Nonnull) token
+                      videoSize:(CGSize) videoSize
+                    videoPreset:(AVCaptureSessionPreset _Nonnull) videoPreset
+useHEVCCodecIfPossibleForRecorder:(BOOL) useHEVCCodecIfPossibleForRecorder
+                    arCloudPath:(NSString * _Nullable) arCloudPath;
+
++ (id _Nullable) getCameraModuleInstance;
++ (BOOL)isTokenExpired;
+
+@end
+NS_ASSUME_NONNULL_END
