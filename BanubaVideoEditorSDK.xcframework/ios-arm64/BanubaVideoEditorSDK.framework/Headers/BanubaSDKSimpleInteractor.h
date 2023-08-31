3dc3ac821b66eff915637bb5ccc1744bdf31a5fa
user <user@users-Mac-mini.local>
2023-08-31 12:50:26 +0300

    automatic framework update

---
+++ BanubaVideoEditorSDK.xcframework/ios-arm64/BanubaVideoEditorSDK.framework/Headers/BanubaSDKSimpleInteractor.h
@@ -0,0 +1,25 @@
+//
+//  BanubaSDKSimpleInteractor.h
+//  BanubaVideoEditorSDK
+//
+//  Created by Andrei Sak on 31.08.23.
+//  Copyright © 2023 Banuba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@import AVKit;
+
+NS_ASSUME_NONNULL_BEGIN
+NS_SWIFT_NAME(BanubaSDKSimpleInteractor)
+
+// MARK: - Banuba SDK Simple framework interactor
+__attribute__((weak_import)) @interface BanubaSDKSimpleInteractor : NSObject
+
++ (id _Nullable) getCameraModuleInstanceWithVideoSize:(CGSize) videoSize
+                                          videoPreset:(AVCaptureSessionPreset _Nonnull) videoPreset
+                    useHEVCCodecIfPossibleForRecorder:(BOOL) useHEVCCodecIfPossibleForRecorder;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- BanubaVideoEditorSDK.xcframework/ios-arm64/BanubaVideoEditorSDK.framework/Headers/BanubaVideoEditorSDK.h
+++ BanubaVideoEditorSDK.xcframework/ios-arm64/BanubaVideoEditorSDK.framework/Headers/BanubaVideoEditorSDK.h
@@ -17,5 +17,6 @@ FOUNDATION_EXPORT const unsigned char BanubaVideoEditorSDKVersionString[];
 #import <BanubaVideoEditorSDK/ARCloudFrameworkInteractor.h>
 #import <BanubaVideoEditorSDK/AudioBrowserFrameworkInteractor.h>
 #import <BanubaVideoEditorSDK/BanubaSDKFrameworkInteractor.h>
+#import <BanubaVideoEditorSDK/BanubaSDKSimpleInteractor.h>
 #import <BanubaVideoEditorSDK/BanubaVideoEditorGallerySDKInteractor.h>
 #import <BanubaVideoEditorSDK/VisualClipVideo.h>
