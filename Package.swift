7665ae4a115b24f35caf3f63872c5524d6510138
user <user@users-Mac-mini.local>
2024-04-11 13:59:33 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -1,17 +1,22 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
+
 import PackageDescription
+
 let package = Package(
   name: "BanubaVideoEditorSDK",
   platforms: [
-    .iOS(.v12)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaVideoEditorSDK",
-      targets: ["BanubaVideoEditorSDK"])
+      targets: ["BanubaVideoEditorSDK"]
+    )
   ],
   targets: [
     .binaryTarget(
       name: "BanubaVideoEditorSDK",
-      path: "BanubaVideoEditorSDK.xcframework")
-  ])
+      path: "BanubaVideoEditorSDK.xcframework"
+    )
+  ]
+)
