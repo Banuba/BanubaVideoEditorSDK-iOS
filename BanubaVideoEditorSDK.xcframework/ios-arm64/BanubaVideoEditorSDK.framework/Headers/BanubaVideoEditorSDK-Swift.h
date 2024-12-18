af0ecaff76e526513133031361edd0957c3b92f3
CI <ci@banuba.com>
2024-12-18 13:31:48 +0300

    automatic framework update

--- BanubaVideoEditorSDK.xcframework/ios-arm64/BanubaVideoEditorSDK.framework/Headers/BanubaVideoEditorSDK-Swift.h
+++ BanubaVideoEditorSDK.xcframework/ios-arm64/BanubaVideoEditorSDK.framework/Headers/BanubaVideoEditorSDK-Swift.h
@@ -308,15 +308,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 
 
 
-
-/// The Composition Timeline configuration
-SWIFT_CLASS("_TtC20BanubaVideoEditorSDK32CompositionTimelineConfiguration")
-@interface CompositionTimelineConfiguration : NSObject
-- (nonnull instancetype)init SWIFT_UNAVAILABLE;
-+ (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
-@end
-
-
 @class NSCoder;
 
 SWIFT_CLASS("_TtC20BanubaVideoEditorSDK13CountdownView")
@@ -402,7 +393,6 @@ SWIFT_CLASS("_TtC20BanubaVideoEditorSDK22ProgressViewController")
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
-
 
 
 
