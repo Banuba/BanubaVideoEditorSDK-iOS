1ffca177ec2fb7aaaba1fe39290382e9e2749184
CI <ci@banuba.com>
2025-03-14 14:02:01 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,18 +2,42 @@
 
 import PackageDescription
 
+let version: Version = "1.40.5"
+
 let package = Package(
   name: "BanubaVideoEditorSDK",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaVideoEditorSDK",
-      targets: ["BanubaVideoEditorSDK"]
+      targets: ["BanubaVideoEditorSDKTarget"]
     )
   ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
+    .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", exact: version),
+    .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", exact: version),
+  ],
   targets: [
+    .target(
+      name: "BanubaVideoEditorSDKWrapper",
+      dependencies: [
+        "BanubaVideoEditorSDK",
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS"),
+        .product(name: "BanubaVideoEditorCore", package: "BanubaVideoEditorCore-iOS"),
+        .product(name: "BanubaLicenseServicingSDK", package: "BanubaLicenseServicingSDK")
+      ],
+      path: "BanubaVideoEditorSDKWrapper"
+    ),
+    .target(
+      name: "BanubaVideoEditorSDKTarget",
+      dependencies: [
+        .target(name: "BanubaVideoEditorSDKWrapper")
+      ],
+      path: "BanubaVideoEditorSDKTarget"
+    ),
     .binaryTarget(
       name: "BanubaVideoEditorSDK",
       path: "BanubaVideoEditorSDK.xcframework"
