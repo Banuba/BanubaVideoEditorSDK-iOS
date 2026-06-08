b14fcc4525f45762da55a11c884993fbc1d1ab4d
CI <ci@banuba.com>
2026-06-08 21:46:30 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,7 +2,8 @@
 
 import PackageDescription
 
-let version: Version = "1.52.1"
+let version: Version = "1.53.0-beta.0"
+let designSystemVersionRange: Range<Version> = "1.0.0"..<"2.0.0"
 
 let package = Package(
   name: "BanubaVideoEditorSDK",
@@ -16,6 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaDesignSystem.git", designSystemVersionRange),
     .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
     .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", exact: version),
     .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", exact: version),
@@ -25,6 +27,7 @@ let package = Package(
       name: "BanubaVideoEditorSDKWrapper",
       dependencies: [
         "BanubaVideoEditorSDK",
+        .product(name: "BanubaDesignSystem", package: "BanubaDesignSystem"),
         .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS"),
         .product(name: "BanubaVideoEditorCore", package: "BanubaVideoEditorCore-iOS"),
         .product(name: "BanubaLicenseServicingSDK", package: "BanubaLicenseServicingSDK")
@@ -40,7 +43,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BanubaVideoEditorSDK",
-      path: "BanubaVideoEditorSDK.xcframework"
+      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaVideoEditorSDK/1.53.0-beta.0/BanubaVideoEditorSDK-1.53.0-beta.0.xcframework.zip",
+      checksum: "687218b2107db7a5bd0feb240c65728baf51139fa68f9be2b6dc16ade4f4565d"
     )
   ]
 )
