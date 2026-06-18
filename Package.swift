3ad013558206a53ca72f64f5d0999c3e3443ca31
CI <ci@banuba.com>
2026-06-18 13:38:57 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,7 +2,8 @@
 
 import PackageDescription
 
-let version: Version = "1.52.1"
+let version: Version = "1.52.2"
+let designSystemVersionRange: Range<Version> = "1.0.0"..<"2.0.0"
 
 let package = Package(
   name: "BanubaVideoEditorSDK",
@@ -16,6 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaDesignSystem.git", designSystemVersionRange),
     .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
     .package(url: "https://github.com/Banuba/BanubaVideoEditorCore-iOS.git", exact: version),
     .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", exact: version),
@@ -25,6 +27,7 @@ let package = Package(
       name: "BanubaVideoEditorSDKWrapper",
       dependencies: [
         "BanubaVideoEditorSDK",
+        .product(name: "BanubaDesignSystem", package: "BanubaDesignSystem"),
         .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS"),
         .product(name: "BanubaVideoEditorCore", package: "BanubaVideoEditorCore-iOS"),
         .product(name: "BanubaLicenseServicingSDK", package: "BanubaLicenseServicingSDK")
