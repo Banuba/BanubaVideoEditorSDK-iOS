a802d97a458cdf071ad21ffc44ec62d9d79861d2
CI <ci@banuba.com>
2026-09-02 18:19:20 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 import PackageDescription
 
 let designSystemVersionRange: Range<Version> = "1.0.4"..<"2.0.0"
-let banubaUtilsVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
-let banubaCoreVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
-let banubaLicenseServicingVersionRange: Range<Version> = "1.54.0"..<"1.59.99"
+let banubaUtilsVersionRange: Range<Version> = "1.54.2"..<"1.59.99"
+let banubaCoreVersionRange: Range<Version> = "1.54.2"..<"1.59.99"
+let banubaLicenseServicingVersionRange: Range<Version> = "1.54.2"..<"1.59.99"
 
 let package = Package(
   name: "BanubaVideoEditorSDK",
@@ -45,8 +45,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "BanubaVideoEditorSDK",
-      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaVideoEditorSDK/1.54.0/BanubaVideoEditorSDK-1.54.0.xcframework.zip",
-      checksum: "ad180c4c8f0b65f90df9b5dc76e1b9bc8f5d8f26327329fbde9adb2fde6eab04"
+      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaVideoEditorSDK/1.54.1/BanubaVideoEditorSDK-1.54.1.xcframework.zip",
+      checksum: "628cf631b91c89bdad517a9ea29c6efc79e98d38f5bfef9e9abf009cf430d8d4"
     )
   ]
 )
